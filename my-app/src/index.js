49240a443f020cba5cdeb057a7ea646e081473bb
ildiko.petovari <ildiko.petovari@gmail.com>
2020-11-11 16:37:10 +0100

    passing dynamic data with props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import StorePicker from "./components/StorePicker";
 import App from "./components/App";
-/*import "./css/style.css";*/
+import "./css/style.css";
 
 render(<App />, document.querySelector('#main'));
