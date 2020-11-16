1414ddc20ba3845916ed394327496c99c0c9c817
ildiko.petovari <ildiko.petovari@gmail.com>
2020-11-16 16:20:19 +0100

    handling events

--- my-app/src/components/StorePicker.js
+++ my-app/src/components/StorePicker.js
@@ -8,19 +8,20 @@ class StorePicker extends React.Component {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. get the text from that input
-    console.log(this);
-
+    const storeName = this.myInput.current.value;
     // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   };
     render() {
-      console.log(this);
       return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" 
-          required 
-          placeholder="Store Name" 
-          defaultValue={getFunName()} 
+          <input 
+              type="text" 
+              ref={this.myInput}
+              required 
+              placeholder="Store Name" 
+              defaultValue={getFunName()} 
           />
           <button type="submit">Visit Store →</button>
         </form>
