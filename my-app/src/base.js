d9d022bbdc8ca71fcf003a007ae50afd92e37dbf
ildiko.petovari <ildiko.petovari@gmail.com>
2020-11-25 08:24:03 +0100

    persisting our state with firebase

--- my-app/src/base.js
+++ my-app/src/base.js
@@ -2,14 +2,14 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAaJ70B0Q86SXfxhJeTArhzZotJBxWnJgI",
-    authDomain: "catch-of-the-day-peto.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-peto.firebaseio.com",
-    /*projectId: "catch-of-the-day-peto",
-    storageBucket: "catch-of-the-day-peto.appspot.com",
-    messagingSenderId: "851977835363",
-    appId: "1:851977835363:web:0c942d4432c2906ef0f956",
-    measurementId: "G-PKDQM48TQ1"*/
+  apiKey: "AIzaSyAaJ70B0Q86SXfxhJeTArhzZotJBxWnJgI",
+  authDomain: "catch-of-the-day-peto.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-peto.firebaseio.com"
+  /*projectId: "catch-of-the-day-peto",
+  storageBucket: "catch-of-the-day-peto.appspot.com",
+  messagingSenderId: "851977835363",
+  appId: "1:851977835363:web:0c942d4432c2906ef0f956",
+  measurementId: "G-PKDQM48TQ1"*/
 });
 
 const base = Rebase.createClass(firebaseApp.database());
--- my-app/src/components/StorePicker.js
+++ my-app/src/components/StorePicker.js
@@ -12,21 +12,21 @@ class StorePicker extends React.Component {
     // 3. Change the page to /store/whatever-they-entered
     this.props.history.push(`/store/${storeName}`);
   };
-    render() {
-      return (
-        <form className="store-selector" onSubmit={this.goToStore}>
-          <h2>Please Enter A Store</h2>
-          <input 
-              type="text" 
-              ref={this.myInput}
-              required 
-              placeholder="Store Name" 
-              defaultValue={getFunName()} 
-          />
-          <button type="submit">Visit Store →</button>
-        </form>
-      );
-    }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
